08f98c7a8e4f6d3dd8a9b5b619e9b5db39236b06
KreLou <45064938+KreLou@users.noreply.github.com>
2019-01-07 10:58:55 +0100

    Basic -APP-Layout
    Adding Header and router to app

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AppRoutingModule} from './app.routing';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
